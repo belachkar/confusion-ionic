cc0bfbda96c7edef257a7844cc976c3f83909732
belachkar <belachkar_ali@hotmail.com>
2020-09-26 19:56:05 +0100

    FAB Favorite

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,13 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { Dish } from '../../shared/dish';
 
-/**
- * Generated class for the DishdetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -19,10 +14,12 @@ export class DishdetailPage {
   errMsg: string;
   avgStars: string;
   numComments: number;
+  isFavorite = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    private favService: FavoriteProvider,
     @Inject('BaseURL') public BaseURL
   ) {
     this._init(navParams);
@@ -30,6 +27,7 @@ export class DishdetailPage {
 
   private _init(navParams: NavParams) {
     this.dish = navParams.get('dish');
+    this.isFavorite = this.favService.isFavorite(this.dish.id);
     this.numComments = this.dish.comments.length;
 
     let total = 0;
@@ -39,6 +37,16 @@ export class DishdetailPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DishdetailPage');
+  }
+
+  addToFavorites() {
+    if (this.isFavorite) {
+      console.log('It\'s already a favorite:', this.dish.id);
+      return false;
+    }
+
+    console.log('Adding to favorite:', this.dish.id);
+    this.isFavorite = this.favService.addFavorite(this.dish.id);
   }
 
 }
--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -1,17 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the FavoriteProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class FavoriteProvider {
+  favorites: Array<any>;
 
   constructor(public http: HttpClient) {
     console.log('Hello FavoriteProvider Provider');
+    this.favorites = [];
+  }
+
+  addFavorite(id: number): boolean {
+    this.favorites.push(id);
+    return true;
+  }
+
+  isFavorite(id: number): boolean {
+    return this.favorites.some(favId => favId === id);
   }
 
 }
